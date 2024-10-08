65677d054a3daa46cb69b23314af5801c18cc589
Alarcon Ace Belen <b.alarconace@gmail.com>
2024-10-09 00:15:04 +0800

    chore: add 5.1 data

--- update.py
+++ update.py
@@ -8,6 +8,5 @@ async def main():
     client.set_assets_repository("blueaxis/enka-data")
     async with client:
         await client.update_assets()
-        # You can see the progress download new assets in console
 
 asyncio.run(main())
